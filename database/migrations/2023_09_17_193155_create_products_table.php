7dc1ffee7c579654e65efeea572a2b6c13c46a1f
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2023-09-18 11:32:24 +0700

    ✨ feat: setup database migration product

---
+++ database/migrations/2023_09_17_193155_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 20)->unique();
+            $table->string('name', 100);
+            $table->string('description', 255)->nullable();
+            $table->decimal('purchase_price', 48, 4);
+            // purchase account
+            $table->decimal('sale_price', 48, 4);
+            // purchase account
+
+            // inventory account 
+
+            $table->integer('stock')->default(0);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
