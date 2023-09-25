e2af8d6c6a200fabc33ab2f1ad402ba90a01dcc2
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2023-09-25 20:35:15 +0700

    ✨ feat: implement create journal

--- app/Services/Transactions/PurchaseService.php
+++ app/Services/Transactions/PurchaseService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Transactions;
 
+use App\Models\Account;
 use App\Models\Journal;
 use App\Models\Product;
 use App\Models\Purchase;
@@ -27,24 +28,92 @@ class PurchaseService
 
         // if no transaction is empty, generate no transaction
         if (empty($inputs['no_transaction'])) {
-            $inputs['no_transaction'] = 'PURCHASE-' . rand(100000, 999999);
+            $inputs['no_transaction'] = rand(100000, 999999);
         }
 
         $purchase_details = $request->purchase_details;
 
-        $purchase = Purchase::create($inputs);
 
+        // journal entry
+        $journal = Journal::create([
+            'no_transaction' => 'J-' . $inputs['no_transaction'],
+            'date' => $inputs['date'],
+            'description' => $inputs['description'] ?? null,
+        ]);
+
+        // get total prices
+        $total_prices = 0;
+        foreach ($purchase_details as $purchase_entry) {
+            $total_prices += $purchase_entry['quantity'] * $purchase_entry['price_per_unit'];
+        }
+
+        // check pay_with_account_id is debit or credit
+        $debit_or_credit = Account::findOrFail($inputs['pay_with_account_id'])->AccountCategory->classification->debit_or_credit;
+
+        // collect data journal details
+        $journal_data = [
+            // the amount in the deposit account decreases
+            [
+                'journal_id' => $journal->id,
+                'account_id' => $inputs['pay_with_account_id'],
+                'debit' => $debit_or_credit == 'debit' ? 0 : $total_prices,
+                'credit' => $debit_or_credit == 'credit' ? 0 : $total_prices,
+            ],
+        ];
+
+        $purchase = Purchase::create([
+            'no_transaction' => 'PURCHASE-' . $inputs['no_transaction'],
+            'journal_id' => $journal->id,
+            'pay_with_account_id' => $inputs['pay_with_account_id'],
+            'supplier_id' => $inputs['supplier_id'],
+            'date' => $inputs['date'],
+            'description' => $inputs['description'] ?? null,
+        ]);
+
+        $journal_data_details = [];
         foreach ($purchase_details as $purchase_detail) {
-            $data = [
+            $product = Product::findOrFail($purchase_detail['product_id']);
+
+            $purchase->purchase_details()->create([
                 'purchase_id' => $purchase->id,
                 'product_id' => $purchase_detail['product_id'],
                 'quantity' => $purchase_detail['quantity'],
                 'price_per_unit' => $purchase_detail['price_per_unit'],
                 'total_price' => $purchase_detail['quantity'] * $purchase_detail['price_per_unit']
-            ];
+            ]);
 
-            $purchase->purchase_details()->create($data);
+
+            // add to journal details (inventory account)
+            $journal_data_details[] = [
+                'journal_id' => $journal->id,
+                'account_id' => $product->inventory_account,
+                'debit' => $product->inventory_debit_or_credit == 'debit' ? $purchase_detail['quantity'] * $purchase_detail['price_per_unit'] : 0,
+                'credit' => $product->inventory_debit_or_credit == 'credit' ? $purchase_detail['quantity'] * $purchase_detail['price_per_unit'] : 0,
+            ];
         }
+
+        // update history stock (create)
+        foreach ($purchase_details as $purchase_detail) {
+            $product = Product::findOrFail($purchase_detail['product_id']);
+
+            $product->productStock()->create([
+                'product_id' => $product->id,
+                'journal_id' => $journal->id,
+                'quantity' => $purchase_detail['quantity'],
+                'type' => 'in',
+            ]);
+        }
+
+        // concat journal data and journal data details
+        $journal_data = collect(array_merge($journal_data, $journal_data_details));
+
+        // insert journal details
+        $journal->journalDetails()->createMany($journal_data);
+
+        $totalDebit = $journal_data->sum('debit');
+        $totalCredit = $journal_data->sum('credit');
+
+        return $purchase;
     }
 
     public function updateData($id, $request)
