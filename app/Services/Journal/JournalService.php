37177d44f5a541970c89cefb3a1e2bb66330ad98
marioapn3 <mario.aprilnino27@gmail.com>
2023-09-28 11:10:23 +0700

    Fix : Journal Detail Delete

--- app/Services/Journal/JournalService.php
+++ app/Services/Journal/JournalService.php
@@ -78,9 +78,9 @@ class JournalService
     public function destroy($id)
     {
         $journal = Journal::findOrFail($id);
-        // $journal_detail = $journal->journalDetails();
+        $journal_detail = $journal->journalDetails();
         $journal->delete();
-        // $journal_detail->delete();
+        $journal_detail->delete();
         return $journal;
     }
 }
